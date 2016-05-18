2f9dc0a0836409cca452c22df54af53210f22d6a
Senny Kalidien <senny@directzichtbaar.nl>
2016-05-18 15:53:18 +0200

    Feature detection call();

--- browser-technologies/assets/js/app.js
+++ browser-technologies/assets/js/app.js
@@ -12,9 +12,9 @@ APP.launcher = (function() {
         document.addEventListener("DOMContentLoaded", function() {
 
             /* Feature Detection */
-            if ((document.querySelectorAll || document.querySelector) && ('forEach' in Array.prototype)) {
+            if ((document.querySelectorAll && document.querySelector) && ('forEach' in Array.prototype && 'call' in Function.prototype)) {
                 APP.data.storage();
-            } else {}
+            }
         });
     };
 
